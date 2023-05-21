9e1a3c1191e8dc8ebc830018f32e4c9bafe3bbfe
Noonfiz <noon.fitzgerald@yahoo.com>
2023-05-21 00:09:16 -0400

    changed the callback URL

--- models/user-model.js
+++ models/user-model.js
@@ -44,7 +44,7 @@ passport.deserializeUser(function(user, cb) {
 passport.use(new GoogleStrategy({
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-   callbackURL: "http://localhost:3000/auth/google/admin-console",
+   callbackURL: "https://drab-puce-slug-gear.cyclic.app/auth/google/admin-console",
   //it has to match with Authorized redirect URIs on the google console
 
 },
